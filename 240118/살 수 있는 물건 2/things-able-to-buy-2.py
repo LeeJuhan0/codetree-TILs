f5469b2eb6689adaa9ce3d031f5eaa28e80d3968
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-01-18 22:30:34 +0900

    Title: 살 수 있는 물건 2, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/살 수 있는 물건 2/things-able-to-buy-2.py
+++ 240118/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -1,9 +1,9 @@
 n = int(input())
 if n >= 3000 :
     print('book')
-if 3000 > n >= 1000 :
+elif n >= 1000 :
     print('mask')
-if 1000 > n >= 500 :
+elif n >= 500 :
     print('pen')
 else :
     print('no')
