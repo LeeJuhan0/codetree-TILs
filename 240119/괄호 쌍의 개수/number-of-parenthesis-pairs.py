684cdabdf7574cb6e611f11e84d94e9f469fc3f4
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-01-19 00:49:41 +0900

    Title: 괄호 쌍의 개수, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240119/괄호 쌍의 개수/number-of-parenthesis-pairs.py
+++ 240119/괄호 쌍의 개수/number-of-parenthesis-pairs.py
@@ -6,11 +6,16 @@ c=0
 for i in range(len(brac)) :
     a=[]
     a = brac[0:i]
-    if a.count('(') < a.count(')') and a.count('(') != a.count(')') :
+    if a.count('(') < a.count(')') :
         c += 1
         a.append('(')
-    elif a.count('(') > a.count(')') and a.count('(') != a.count(')') :
+    elif a.count('(') > a.count(')') :
         c +=1
-
+    
 c -= 1 #맨마지막 괄호
+
+if brac.count('(') == brac.count(')') :
+    c=0
+
+
 print(c)
