cefb552dbb10d58004c0d0870ef50f59035ab5ff
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-01-19 22:13:19 +0900

    Title: 숫자로 이루어진 사각형, Time: 524ms, Memory: 157MB, Status: Wrong Answer - CodeTree

--- 240119/숫자로 이루어진 사각형/rectangle-with-a-number.py
+++ 240119/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -1,8 +1,9 @@
+number=[]
+for j in range(1000000):
+    for i in range(1,10) :
+        number.append(i)
+
 def numberrectangle(n) :
-    number=[]
-    for j in range(10000000):
-        for i in range(1,10) :
-            number.append(i)
     for k in range(n):
         for i in range(4*k,n+4*k):
             print(number[i], end=" ")
