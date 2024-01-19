06973de45396f762825f94d607130b838854abc2
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-01-19 20:44:17 +0900

    Title: 999 또는 -999, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240119/999 또는 -999/999-or-999.py
@@ -0,0 +1,14 @@
+num = list(map(int, input().split()))
+
+maximum = -998
+minimum = 998
+
+for i in num :
+    if i > maximum and i < 999:
+        maximum = i
+
+for j in num :
+    if j < minimum :
+        minimum = j
+
+print(f'{maximum} {minimum}')
