a8020f903824fff514cc6c3e620ef56505c26f4f
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-01-21 00:45:30 +0900

    Title: 정수의 최솟값, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240121/정수의 최솟값/minimum-value-of-an-integer.py
+++ 240121/정수의 최솟값/minimum-value-of-an-integer.py
@@ -1,6 +1,8 @@
-def minist(a, b, c,) :
+def minist(a, b, c) :
     empty_list = []
-    empty_list.append(a, b, c)
+    empty_list.append(a)
+    empty_list.append(b)
+    empty_list.append(c)
     print(min(empty_list))
 
 A, B, C = map(int, input().split())
