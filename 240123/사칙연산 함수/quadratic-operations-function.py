90fc6922425283dbbcdb7a316c4d336dcf9f08c7
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-01-23 18:42:34 +0900

    Title: 사칙연산 함수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240123/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,19 @@
+a, b, c = map(str, input().split())
+
+def calculator(a, b, c) :
+    mul = int(a)*int(c)
+    dvi = int(a)/int(c)
+    plu = int(a)+int(c)
+    miu = int(a)-int(c)
+    if b == '+' :
+        print(f'{a} + {c} = {plu}')
+    elif b == '-' :
+        print(f'{a} - {c} = {miu}')
+    elif b == '/' :
+        print(f'{a} / {c} = {dvi}')
+    elif b == '*' :
+        print(f'{a} * {c} = {mul}')
+    else :
+        print('False')
+
+calculator(a, b, c)
