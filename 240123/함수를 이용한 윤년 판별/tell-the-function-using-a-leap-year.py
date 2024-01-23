817c460e77e5aeb72a75e04f730bd5aa991286d6
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-01-23 17:51:27 +0900

    Title: 함수를 이용한 윤년 판별, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240123/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240123/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,9 +1,9 @@
 def leap_year(n) :
     if n % 4 != 0 :
         return False
-    elif n % 4 == 0 and n % 100 :
+    elif n % 4 == 0 and n % 100 == 0 :
         return False
-    elif n % 4 == 0 and n % 100 and n % 400 :
+    elif n % 4 == 0 and n % 100 == 0 and n % 400 = 0 :
         return True
     else :
         return False
