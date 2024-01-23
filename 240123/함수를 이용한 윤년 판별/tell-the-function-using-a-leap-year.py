21137171f02123e83d45bfc45a41b80c7862fa83
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-01-23 18:02:09 +0900

    Title: 함수를 이용한 윤년 판별, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240123/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,7 +1,7 @@
 def leap_year(n) :
     if n % 4 != 0 :
         return False
-    elif n % 4 == 0 and n % 100 == 0 :
+    elif n % 4 == 0 and n % 100 == 0 and n % 400 != 0:
         return False
     elif n % 4 != 0 and n % 100 != 0 and n % 400 != 0 :
         return False
