e5e6256dcd688c89a9c4222f489e4c78dde76211
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-01-27 22:31:35 +0900

    Title: 특정 구간의 합, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240127/특정 구간의 합/sum-of-specific-intervals.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+def asd():
+    global A
+    for i in range(m):
+        global ai 
+        list1=[]
+        ai = list(map(int, input().split()))
+        print(sum(A[ai[0]-1:ai[1]]))
+        
+
+asd()
