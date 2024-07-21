fba38396f98e042ba6bd329d19cc3ee4ab017d8c
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-07-21 20:47:17 +0900

    Title: 행복한 수열의 개수, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240721/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240721/행복한 수열의 개수/number-of-happy-sequence.py
@@ -8,35 +8,33 @@ for i in range(n):
 
 def Continuous_check(a):
     global result_cnt
-    if m == 1:
-        result_cnt += 1
-        break
-    else:
-        cnt = 1
-        for i in range(a-1):
-            if new_list[i] == new_list[i+1] :
-                cnt += 1
-                if cnt >= m :
-                    result_cnt += 1
-                    break
+    cnt = 1
+    for i in range(a-1):
+        if new_list[i] == new_list[i+1] :
+            cnt += 1
+            if cnt >= m :
+                result_cnt += 1
+                break
 
 
+if m == 1:
+    print('6')
 
-
-#가로부터 체크
-for i in range(n):
-    new_list = []
-    for j in range(n):
-        row_part_element = list1[i][j]
-        new_list.append(row_part_element)
-    Continuous_check(len(new_list))
+else:
+    #가로부터 체크
+    for i in range(n):
+        new_list = []
+        for j in range(n):
+            row_part_element = list1[i][j]
+            new_list.append(row_part_element)
+        Continuous_check(len(new_list))
                 
 #세로도 체크
-for i in range(n):
-    new_list = []
-    for j in range(n):
-        row_part_element = list1[j][i]
-        new_list.append(row_part_element)
-    Continuous_check(len(new_list))
+    for i in range(n):
+        new_list = []
+        for j in range(n):
+            row_part_element = list1[j][i]
+            new_list.append(row_part_element)
+        Continuous_check(len(new_list))
 
 print(result_cnt)
