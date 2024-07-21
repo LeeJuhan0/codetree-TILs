487698252ca58a80ec5e3a61a22a435ddb9646c9
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-07-21 20:27:53 +0900

    Title: 행복한 수열의 개수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240721/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240721/행복한 수열의 개수/number-of-happy-sequence.py
@@ -2,31 +2,34 @@ n, m = map(int, input().split())
 list1 = []
 result_cnt = 0
 
-# Read the matrix
 for i in range(n):
     row = list(map(int, input().split()))
     list1.append(row)
 
 def Continuous_check(a):
-    global result_cnt  # Use the global variable
-    cnt = 1  # Initialize count to 1 since we count the first occurrence
-    for i in range(1, a):
-        if new_list[i] == new_list[i - 1]:
+    global result_cnt
+    cnt = 1
+    for i in range(a-1):
+        if new_list[i] == new_list[i+1] :
             cnt += 1
-            if cnt == m:
+            if cnt >= m :
                 result_cnt += 1
                 break
-        else:
-            cnt = 1
 
-# Check horizontally
+#가로부터 체크
 for i in range(n):
-    new_list = list1[i]
+    new_list = []
+    for j in range(n):
+        row_part_element = list1[i][j]
+        new_list.append(row_part_element)
     Continuous_check(len(new_list))
-
-# Check vertically
+                
+#세로도 체크
 for i in range(n):
-    new_list = [list1[j][i] for j in range(n)]
+    new_list = []
+    for j in range(n):
+        row_part_element = list1[j][i]
+        new_list.append(row_part_element)
     Continuous_check(len(new_list))
 
 print(result_cnt)
