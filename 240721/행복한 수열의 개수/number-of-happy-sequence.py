07b43a08afff396bba73c24c6eb9b0f743b958d0
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-07-21 20:44:38 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240721/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240721/행복한 수열의 개수/number-of-happy-sequence.py
@@ -16,7 +16,7 @@ def Continuous_check(a):
         for i in range(a-1):
             if new_list[i] == new_list[i+1] :
                 cnt += 1
-                elif cnt >= m :
+                if cnt >= m :
                     result_cnt += 1
                     break
 
