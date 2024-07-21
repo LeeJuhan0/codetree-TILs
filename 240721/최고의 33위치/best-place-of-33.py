b5d8e9c7472aa8f4910728191b989366dee3fd49
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2024-07-21 18:24:14 +0900

    Title: 최고의 33위치, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240721/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,18 @@
+n = int(input())
+list1 = []
+result_list = []
+
+for i in range(n):
+    horizontal = list(map(int, input().split()))
+    list1.append(horizontal)
+
+
+for k in range(n-2):
+    cnt_of_1 = 0
+    for i in range(3):
+        for j in range(3):
+            if list1[i+k][j+k] == 1:
+                cnt_of_1 += 1
+    result_list.append(cnt_of_1)
+
+print(max(result_list))
