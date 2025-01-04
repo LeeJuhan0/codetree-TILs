5f3359178385812389d9138f92102f3b569a5a38
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-04 13:18:05 +0900

    Title: 입력받아 계산 4, Time: 106ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250104/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+
+        a = sc.nextInt();
+        
+        System.out.println(2 * a);
+        
+        // Please write your code here.
+    }
+}
