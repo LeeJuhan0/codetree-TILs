716b50191c0c3bf5197d4cb700f079467161a6f9
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-05 20:52:30 +0900

    Title: a/b 출력, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250105/a/b 출력/a-divide-b.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(a/b+ ".");
+
+
+        for (int i = 0; i <20 ; i++) {
+            a %= b;
+            a *= 10;
+            System.out.print(a/b);
+
+            
+        }
+    }
+}
