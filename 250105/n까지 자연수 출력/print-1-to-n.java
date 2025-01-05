948e240b1eee76e065fbff19d6dda4093d08e437
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-05 18:01:33 +0900

    Title: n까지 자연수 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250105/n까지 자연수 출력/print-1-to-n.java
+++ 250105/n까지 자연수 출력/print-1-to-n.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int c = a ;
 
         while (a >= 1) {
-            System.out.print((11-a) + " ");
+            System.out.print((c-(a-1)) + " ");
             a--;
         }
     }
