2740d1e2f0a6a914774b24987df33aa39c527ebe
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-05 23:20:36 +0900

    Title: 등장횟수 확인, Time: 106ms, Memory: 10MB, Status: Passed - CodeTree

--- 250105/등장횟수 확인/check-number-of-appearances.java
+++ 250105/등장횟수 확인/check-number-of-appearances.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int cnt = 0;
 
-        for (int i=1, i <=5; i++) {
+        for (int i=1; i <=5; i++) {
             int a = sc.nextInt();
             if (a % 2==0) {
                 cnt += 1;
