6c663d2947741757619f7eaa56c6e3bf8eb5f256
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-07 14:20:02 +0900

    Title: 별표 출력하기 9, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250107/별표 출력하기 9/print-star-9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1 ; i <= n ; i++) {
+            for (int j = n - i ; j > 0 ; j--) {
+                System.out.print("  ");
+            }
+
+            for (int j = i*2 -1 ; j > 0 ; j--) {
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+
+        }
+    }
+}
