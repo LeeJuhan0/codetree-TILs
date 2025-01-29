fb3e3a950e4cae145fdfb60c10dcf0307c262f10
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-29 22:23:38 +0900

    Title: 이상한 진수 2, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

--- 250129/이상한 진수 2/awkward-digits-2.java
+++ 250129/이상한 진수 2/awkward-digits-2.java
@@ -6,17 +6,26 @@ public class Main {
         String n = sc.next();
         int Value = 0;
         int[] arr = new int[n.length()];
+        boolean nonzero = true;
         
         for(int i = 0 ; i < n.length() ; i++) {
             arr[i] =  n.charAt(i);
-            
         }
 
         for(int i = 0 ; i < n.length() ; i++) {
             if (arr[i] =='0') {
                 arr[i] = '1';
+                nonzero = false;
                 break;
             }      
+        }
+        if (nonzero) {
+            for (int i = n.length()-1 ; i >= 0 ; i--) {
+                if (arr[i] == '1') {
+                    arr[i] = '0';
+                    break;
+                } 
+            }
         }
         
         for(int i = 0 ; i < n.length() ; i++) {
