c009ac697ca90e6c8b9f40ee9d683d9ce7b3d2c6
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-30 21:22:50 +0900

    Title: 최고의 13위치 2, Time: 139ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250130/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int curcnt = 0, maxcnt = 0;
+        for(int i = 0; i < n-1 ; i++) {
+            for(int j = 0; j < n-2 ; j++) {
+                for(int k = i+1 ; k < n ; k++) {
+                    for(int l = 0; l < n-2 ; l++) {
+                        curcnt = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][l] +arr[k][l+1] + arr[k][l+2];
+                        maxcnt = Math.max(curcnt, maxcnt);
+                        curcnt = 0;
+                    }
+                }
+            }
+        }
+        if(n>5) {
+            for(int i = 0; i < n ; i++) {
+                for(int j = 0; j < n-3 ; j++) {
+                    for(int l = j+3; l < n ; l++) {
+                        curcnt = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i][l] +arr[i][l+1] + arr[i][l+2];
+                        maxcnt = Math.max(curcnt, maxcnt);
+                        curcnt = 0;   
+                    }
+                }
+            }
+        }
+        System.out.print(maxcnt);
+    }
+}
