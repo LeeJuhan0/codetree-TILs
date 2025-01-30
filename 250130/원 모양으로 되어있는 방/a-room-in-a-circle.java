d8b892ccfce57f5b8c4bf4ebb7479edf5d7e093a
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-30 14:09:13 +0900

    Title: 원 모양으로 되어있는 방, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250130/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] roomarr = new int[n];
+        int[] rotatearr = new int[n];
+        for (int i = 0 ; i < n ; i++) {
+            roomarr[i] = sc.nextInt();
+            rotatearr[i] = roomarr[i];
+        }
+        
+        int mincnt = Integer.MAX_VALUE;
+        int curcnt = 0 ;
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < n ; j++) {
+                curcnt += j*rotatearr[j];
+            }
+            mincnt = Math.min(curcnt, mincnt);
+            curcnt = 0 ;
+            for (int j = 0 ; j < n ; j++) {
+                rotatearr[j] = roomarr[(i+j)%n];
+            }   
+        }
+        System.out.print(mincnt);
+    }
+}
