bafbd7c4aeb8b0e01363b02a8b252dc9a83337ef
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-30 20:22:48 +0900

    Title: 오목, Time: 129ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 250130/오목/O-mok.java
+++ 250130/오목/O-mok.java
@@ -63,6 +63,23 @@ public class Main {
                 }
             }
         }
+        //-1 diagonal 5conti
+        for(int i = 4 ; i < 19 ; i++) { //row
+            for(int j = 0; j < 19-4; j++ ) { // col 
+                if(arr[i][j] == 1 && arr[i-1][j+1] == 1 && arr[i-2][j+2] == 1 && arr[i-3][j+3] == 1 && arr[i-4][j+4] == 1) {
+                    win_num = 1;
+                    win_center_x_idx = j+2+1 ;
+                    win_center_y_idx = i-2+1 ;
+                    flag0 = false;
+                }
+                else if(arr[i][j] == 2 && arr[i-1][j+1] == 2 && arr[i-2][j+2] == 2 && arr[i-3][j+3] == 2 && arr[i-4][j+4] == 2) {
+                    win_num = 2;
+                    win_center_x_idx = j+2+1 ;
+                    win_center_y_idx = i-2+1 ;
+                    flag0 = false;
+                }
+            }
+        }
         System.out.println((flag0) ? 0 : win_num);
         System.out.print(win_center_y_idx + " " + win_center_x_idx);
     }
