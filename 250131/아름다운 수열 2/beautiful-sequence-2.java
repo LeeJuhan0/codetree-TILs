7edd579a5e5c078544470887cb0fbf73b70a63ad
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-01-31 14:26:34 +0900

    Title: 아름다운 수열 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250131/아름다운 수열 2/beautiful-sequence-2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arrA = new int[n];
+        int[] arrB = new int[m];
+        for(int i = 0 ; i < n ; i++){
+            arrA[i] = sc.nextInt();
+        }
+        for(int i = 0 ; i < m ; i++){
+            arrB[i] = sc.nextInt();
+        }
+        int ans = 0;
+        for(int i = 0 ; i <= n-m ; i++){
+            int[] arr_B = arrB.clone();
+            int cnt = 0;
+            for(int j = i ; j < i+m ; j++){
+               for(int k = 0 ; k < m ; k++){
+                    if(arr_B[k] == arrA[j]){
+                        arr_B[k] = -1;
+                        cnt++;
+                    }
+                }
+            }
+            if(cnt == m){
+                 ans++;
+            }
+        }
+        System.out.print(ans);
+    }
+}
