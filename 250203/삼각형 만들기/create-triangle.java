0f5087588ca42505bbd01db3a4ad21578553bb1b
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-02-03 16:43:38 +0900

    Title: 삼각형 만들기, Time: 169ms, Memory: 11MB, Status: Passed - CodeTree

--- 250203/삼각형 만들기/create-triangle.java
+++ 250203/삼각형 만들기/create-triangle.java
@@ -13,8 +13,10 @@ public class Main {
         for(int i = 0 ; i < n-2 ; i++) {
             for(int j = i+1 ; j < n-1 ; j++) {
                 for(int k = j+1 ; k < n ; k++) {
-                    area = arr[0][i]*arr[1][j]+arr[0][j]*arr[1][k]+arr[0][k]*arr[1][i]-(arr[0][j]*arr[1][i]+arr[0][k]*arr[1][j]+arr[0][i]*arr[1][k]);
-                    max_area = Math.max(area, max_area);
+                    if(((arr[0][i]-arr[0][j]) == 0 || (arr[0][i]- arr[0][k]) == 0 || (arr[0][j] - arr[0][k])==0) && (arr[1][i]-arr[1][j] == 0 || arr[1][i]- arr[1][k] == 0 || arr[1][j] - arr[1][k] == 0)) {
+                        area = Math.abs((arr[0][i]*arr[1][j]+arr[0][j]*arr[1][k]+arr[0][k]*arr[1][i])-(arr[0][j]*arr[1][i]+arr[0][k]*arr[1][j]+arr[0][i]*arr[1][k]));
+                        max_area = Math.max(area, max_area);
+                    }
                 }
             }
         }
