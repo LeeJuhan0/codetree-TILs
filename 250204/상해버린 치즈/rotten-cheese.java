3ee2350c59de5cab44e2b4f4e6de3f19aaca323d
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-02-04 01:19:53 +0900

    Title: 상해버린 치즈, Time: 175ms, Memory: 12MB, Status: Passed - CodeTree

--- 250204/상해버린 치즈/rotten-cheese.java
+++ 250204/상해버린 치즈/rotten-cheese.java
@@ -3,78 +3,96 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); //n명
-        int m = sc.nextInt(); //치즈의 개수
-        int d = sc.nextInt(); //먹은 기록
-        int s = sc.nextInt(); //아픈 기록
-        int[][] d_arr = new int[d][3];
-        int[][] s_arr = new int[s][2];
-        for(int i = 0 ; i < d ; i++) {
-            d_arr[i][0] = sc.nextInt(); // 먹은사람번호
-            d_arr[i][1] = sc.nextInt(); // 먹은치즈번호
-            d_arr[i][2] = sc.nextInt(); // 먹은시간
+        int n = sc.nextInt(); // Number of people
+        int m = sc.nextInt(); // Number of cheese types
+        int d = sc.nextInt(); // Eating records
+        int s = sc.nextInt(); // Sickness records
+        
+        int[][] d_arr = new int[d][3]; // Eating records array
+        int[][] s_arr = new int[s][2]; // Sickness records array
+        
+        // Reading eating records
+        for (int i = 0; i < d; i++) {
+            d_arr[i][0] = sc.nextInt(); // Person number
+            d_arr[i][1] = sc.nextInt(); // Cheese number
+            d_arr[i][2] = sc.nextInt(); // Eating time
         }
-        for(int i = 0 ; i < s ; i++) {
-            s_arr[i][0] = sc.nextInt(); // 아픈사람번호
-            s_arr[i][1] = sc.nextInt(); // 배아픔발생시간
+        
+        // Reading sickness records
+        for (int i = 0; i < s; i++) {
+            s_arr[i][0] = sc.nextInt(); // Sick person number
+            s_arr[i][1] = sc.nextInt(); // Time of sickness
         }
-        int[] havepillnum = new int[n+1]; 
-        int[] spoiled_cheese_num = new int[m+1];
-        for(int i = 0 ; i < d ; i++) {  //상했을 확률이 있는 치즈찾기
-            for(int j = 0 ; j < s ; j++) {
-                if(d_arr[i][0] == s_arr[j][0] && d_arr[i][2] <= s_arr[j][1] - 1) {
-                    spoiled_cheese_num[d_arr[i][1]]=1;
-                }                
-            }
-        }
-
-        for(int i = 0 ; i < s ; i++) { //아픈사람이 아픈 이후 먹은 치즈제외
-            for(int j = 0 ; j < d ; j++) {
-                if(d_arr[j][0] == s_arr[i][0] && d_arr[j][2] >= s_arr[i][1]) {
-                    spoiled_cheese_num[d_arr[j][1]]=0;
-                    for(int k = 0 ; k < d ; k++) {
-                        if(d_arr[k][1] == d_arr[j][1] && d_arr[k][2] <= s_arr[i][1] - 1) { //이전에 먹은 치즈는 상한상태 유지
-                            spoiled_cheese_num[d_arr[k][1]]=1;
-                        }
-                    } 
+        
+        int[] havepillnum = new int[n + 1];
+        int[] spoiled_cheese_num = new int[m + 1];
+        
+        // Identify potentially spoiled cheese
+        for (int i = 0; i < d; i++) {
+            for (int j = 0; j < s; j++) {
+                if (d_arr[i][0] == s_arr[j][0] && d_arr[i][2] <= s_arr[j][1] - 1) {
+                    spoiled_cheese_num[d_arr[i][1]] = 1;
                 }
             }
         }
-    
         
-        int[] arr = new int[m+1];
-        for(int i = 0 ; i < s ; i++) { //아픈사람이 안먹은 치즈제외
-            for(int j = 0 ; j < d ; j++) {
-                if(d_arr[j][0] == s_arr[i][0]) { 
+        // Refine spoiled cheese identification by excluding cheese eaten only after sickness
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j < d; j++) {
+                if (d_arr[j][0] == s_arr[i][0] && d_arr[j][2] >= s_arr[i][1]) {
+                    boolean ateBeforeSick = false;
+                    for (int k = 0; k < d; k++) {
+                        if (d_arr[k][0] == s_arr[i][0] && d_arr[k][1] == d_arr[j][1] && d_arr[k][2] < s_arr[i][1]) {
+                            ateBeforeSick = true;
+                            break;
+                        }
+                    }
+                    if (!ateBeforeSick) {
+                        spoiled_cheese_num[d_arr[j][1]] = 0;
+                    }
+                }
+            }
+        }
+        
+        int[] arr = new int[m + 1];
+        
+        // Count cheese eaten by sick people
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j < d; j++) {
+                if (d_arr[j][0] == s_arr[i][0]) {
                     arr[d_arr[j][1]]++;
                 }
             }
         }
-
-        for(int i = 1 ; i < m+1 ; i++) {
-            if(arr[i] < s) {
+        
+        // Exclude cheese not eaten by all sick people
+        for (int i = 1; i <= m; i++) {
+            if (arr[i] < s) {
                 spoiled_cheese_num[i] = 0;
             }
         }
-
-        int ans = 0, max_ans =0;
-        for(int i = 1 ; i < m+1 ; i++) {
-            if(spoiled_cheese_num[i] == 1) {
-                for(int j = 0 ; j < d ; j++) {
+        
+        int ans = 0, max_ans = 0;
+        
+        // Count people who ate spoiled cheese
+        for (int i = 1; i <= m; i++) {
+            if (spoiled_cheese_num[i] == 1) {
+                for (int j = 0; j < d; j++) {
                     if (d_arr[j][1] == i) {
                         havepillnum[d_arr[j][0]]++;
                     }
                 }
-                for(int k = 1 ; k < n+1 ; k++) {
-                    if(havepillnum[k] != 0) {
-                    ans++;
-                    }     
-                    havepillnum[k] = 0 ;
+                for (int k = 1; k <= n; k++) {
+                    if (havepillnum[k] != 0) {
+                        ans++;
+                    }
+                    havepillnum[k] = 0;
                 }
+                max_ans = Math.max(ans, max_ans);
+                ans = 0;
             }
-            max_ans = Math.max(ans,max_ans);
-            ans = 0;
         }
+        
         System.out.print(max_ans);
     }
 }
