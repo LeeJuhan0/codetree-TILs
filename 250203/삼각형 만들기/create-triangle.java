fa2d577e8441b77c0b06ae79b0d1f6c8ba008dff
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-02-03 16:30:07 +0900

    Title: 삼각형 만들기, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250203/삼각형 만들기/create-triangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[2][n];
+        for(int i = 0 ; i < n ; i++) {
+            arr[0][i] = sc.nextInt();
+            arr[1][i] = sc.nextInt();
+        }
+        int max_area = 0, area;
+        for(int i = 0 ; i < n-2 ; i++) {
+            for(int j = i+1 ; j < n-1 ; j++) {
+                for(int k = j+1 ; k < n ; k++) {
+                    area = arr[0][i]*arr[1][j]+arr[0][j]*arr[1][k]+arr[0][k]*arr[1][i]-(arr[0][j]*arr[1][i]+arr[0][k]*arr[1][j]+arr[0][i]*arr[1][k]);
+                    max_area = Math.max(area, max_area);
+                }
+            }
+        }
+        System.out.print(max_area);
+    }
+}
