aee62fecee4135403fd740cfa912978f044d1920
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-02-05 21:30:19 +0900

    Title: 이상한 폭탄 2, Time: 134ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250205/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[100];
+        int max_num = -1, cur_num = 0;
+
+        for(int i = 0 ; i < n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = i+1 ; j < i+k+1 ; j++) {
+                if(arr[i] == arr[j]) {
+                    cur_num = arr[i];
+                    max_num = Math.max(max_num, cur_num);
+                }
+            }
+        }
+        System.out.print(max_num);
+    }
+}
