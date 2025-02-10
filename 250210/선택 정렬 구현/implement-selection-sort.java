dde05bda06ec3b0982ac91740a9601203eaabc3b
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-02-10 13:53:34 +0900

    Title: 선택 정렬 구현, Time: 153ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250210/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for(int i = 0 ; i < n ; i++) {
+            int min = 100, idx = -1;
+            for(int j = i ; j < n ; j++) {
+                if(arr[j] < min){
+                    min = arr[j];
+                    idx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = min;
+            arr[idx] = temp;
+        }
+        for(int i = 0 ; i < n ; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
