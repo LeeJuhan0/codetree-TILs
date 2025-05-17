dfb8dbc6ba58fb5c806710c3eab36a2eb94cf0d6
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-05-17 13:33:46 +0900

    Title: 동전 더하기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250517/동전 더하기/add-coins.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+        int cnt = 0;
+        for (int i = n-1; i >= 0 ; i--) {
+            if (k/coins[i] > 0) {
+                cnt += k/coins[i];
+                k -= coins[i]*(k/coins[i]);
+            } 
+        }
+        System.out.print(cnt);
+    }
+}
