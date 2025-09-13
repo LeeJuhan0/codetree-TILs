f45bf40d5f0823316ad7ad7dd8465079d30670ba
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-09-14 01:24:50 +0900

    Title: 피보나치 수, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250914/피보나치 수/fibonacci-number.py
+++ 250914/피보나치 수/fibonacci-number.py
@@ -1,10 +1,8 @@
 N = int(input())
 
 # Please write your ode here.
-def fibo(N):
-    if N == 1 :
-        return 1
-    elif N == 2:
-        return 1
-    return fibo(N-1) + fibo(N-2)
-print(fibo(N))
+fibo = [1] * 45
+for i in range(2,N):
+    fibo[i] = fibo[i-1] + fibo[i-2]
+
+print(fibo[N-1])
