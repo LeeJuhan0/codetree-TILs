e9e18984bb95e8d9a9ef588c359c02a0d85def39
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-09-14 01:58:09 +0900

    Title: 사각형 채우기 3, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250914/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+# Please write your code here.
+square = [0] * 1001
+square[0] = 1
+square[1] = 2
+square[2] = 7
+square[3] = 22
+square[4] = 71
+
+for i in range(3,n+1) :
+    square[i] = square[i-1]*3+square[i-2]-square[i-3]
+
+print(square[n])
