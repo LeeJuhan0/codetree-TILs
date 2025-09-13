229f452dd6a08468491171417e27957d4bb1b4f5
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-09-14 01:50:58 +0900

    Title: 사각형 채우기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250914/사각형 채우기/rectangle-fill.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+# Please write your code here.
+squar = [0] * 1001
+squar[0] = 0
+squar[1] = 1
+squar[2] = 2
+squar[3] = 3
+squar[4] = 5
+
+for i in range(4,n+1) :
+    squar[i] = squar[i-2]*2 + squar[i-3]
+print(squar[n])
