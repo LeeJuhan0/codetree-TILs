d55128ff784a69e08cd7495a4169de5f3389b235
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-09-15 17:08:42 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 63ms, Memory: 17MB, Status: Passed - Codetree

--- 250915/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 250915/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -7,13 +7,12 @@ dp = [[0]*n for i in range(n)]
 dp[0][0] = grid[0][0]
 
 
-for i in range(n) :
-    dp[i][0] = grid[i][0]
-    dp[0][i] = grid[0][i]
+for i in range(1,n) :
+    dp[i][0] = min(dp[i-1][0], grid[i][0])
+    dp[0][i] = min(dp[0][i-1], grid[0][i])
 
 for i in range(1,n) :
     for j in range(1,n) :
-        dp[i][j] = min(min(dp[i-1][j],dp[i][j-1]),grid[i][j])
+        dp[i][j] = min(max(dp[i-1][j],dp[i][j-1]),grid[i][j])
 
-
-print(min(max(dp[n-2][n-1],dp[n-1][n-2]),grid[n-1][n-1],grid[0][0]))
+print(dp[n-1][n-1])
