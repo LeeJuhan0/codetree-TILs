2e8077eb5ffcd30749c2d15bc7b751c6d6c829de
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2026-02-18 18:35:22 +0900

    Title: 그래프 탐색, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260218/그래프 탐색/graph-traversal.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+graph = [[0]*(n+1) for _ in range(n+1)]
+for i in edges:
+    graph[i[0]][i[1]] = 1
+    graph[i[1]][i[0]] = 1
+
+visited = [False] *(n+1)
+
+def dfs(vertax) :
+    for curr_v in range(1,n+1):
+        if graph[vertax][curr_v] and not visited[curr_v] :
+            visited[curr_v] = True
+            dfs(curr_v)
+
+dfs(1)
+print(visited.count(True)-1)
+
+# Please write your code here.
