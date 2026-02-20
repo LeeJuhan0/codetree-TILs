2e611e9abc68f7fa43dfd3dfe5be2c2eef7564b0
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2026-02-20 13:37:03 +0900

    Title: 안전 지대, Time: 82ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260220/안전 지대/comfort-zone.py
@@ -0,0 +1,38 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+section_by_k = []
+
+def can_go(x,y,k):
+    if not in_range(x,y):
+        return False
+    if visited[x][y] == 1 or grid[x][y] <= k :
+        return False 
+    return True
+
+def in_range(x,y):
+    if 0 <= x and x < n and 0 <= y and y < m:
+        return True
+    return False
+
+def dfs(x,y,k):
+    visited[x][y] = 1
+    dxs, dys = [-1,1,0,0], [0,0,-1,1]
+    for dx, dy in zip(dxs, dys):
+        next_x, next_y = x + dx, y + dy
+        if can_go(next_x, next_y, k) :
+            dfs(next_x, next_y, k)
+    return 1
+
+for k in range(1,max(map(max, grid))):
+    visited = [[0 for _ in range(m)] for _ in range(n)]
+    cnt = 0
+    for x in range(n):
+        for y in range(m):
+            if can_go(x,y,k):
+                cnt += dfs(x,y,k)
+    section_by_k.append([cnt,k])
+
+
+section_by_k.sort(key = lambda x:(x[0], x[1]))
+print(*section_by_k[-1])
+
